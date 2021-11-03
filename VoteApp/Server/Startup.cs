7db0d6d2b508a42118bd4080f1efa5f2a2c591ac
andlekbra <89541859+andlekbra@users.noreply.github.com>
2021-11-03 21:06:05 +0100

    changedDb (#11)
    Co-authored-by: Anders Lekve Brandseth <ALB@teknotherm.no>

--- VoteApp/Server/Startup.cs
+++ VoteApp/Server/Startup.cs
@@ -33,7 +33,9 @@ namespace VoteApp.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(
+        Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddDatabaseDeveloperPageExceptionFilter();
 
